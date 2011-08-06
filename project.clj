ca36717bf6272a48b63d4d646609f58dcf71efe5
Josh Comer <joshjcomer@gmail.com>
2011-08-06 10:53:53 -0300

    Upped the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljcolour "1.0.2"
+(defproject cljcolour "1.0.3"
   :description "Add some colour to your output"
   :url "https://github.com/jjcomer/cljcolour"
   :dependencies [[org.clojure/clojure "1.2.1"]])
