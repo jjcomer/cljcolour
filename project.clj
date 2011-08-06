0d49b96fa36eff83dffb6a37d73cbf7989535d85
Josh Comer <joshjcomer@gmail.com>
2011-08-06 07:49:02 -0300

    Fixed namespace issues

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljcolour "1.0.0-SNAPSHOT"
+(defproject cljcolour "1.0.2"
   :description "Add some colour to your output"
   :url "https://github.com/jjcomer/cljcolour"
   :dependencies [[org.clojure/clojure "1.2.1"]])
