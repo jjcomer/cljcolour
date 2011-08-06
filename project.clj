1db763dae5ea0e9f44494e0af433176ff8a1884b
Josh Comer <joshjcomer@gmail.com>
2011-08-06 11:10:29 -0300

    Fixed colour codes… again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljcolour "1.0.3"
+(defproject cljcolour "1.0.4"
   :description "Add some colour to your output"
   :url "https://github.com/jjcomer/cljcolour"
   :dependencies [[org.clojure/clojure "1.2.1"]])
--- src/cljcolour.clj
+++ src/cljcolour.clj
@@ -2,22 +2,22 @@
       :author "Josh Comer <joshjcomer@gmail.com>"}
     cljcolour)
 
-(def black {:fore "2;30" :back "22;40"})
-(def red {:fore "2;31" :back "22;41"})
-(def green {:fore "2;32" :back "22;42"})
-(def yellow {:fore "2;33" :back "22;43"})
-(def blue {:fore "2;34" :back "22;44"})
-(def purple {:fore "2;35" :back "22;45"})
-(def cyan {:fore "2;36" :back "22;46"})
-(def lightgrey {:fore "2;37" :back "22;47"})
-(def darkgrey {:fore "01;30" :back "1;40"})
-(def boldred {:fore "01;31" :back "1;41"})
-(def boldgreen {:fore "01;32" :back "1;42"})
-(def boldyellow {:fore "01;33" :back "1;43"})
-(def boldblue {:fore "01;34" :back "1;44"})
-(def boldpurple {:fore "01;35" :back "1;45"})
-(def boldcyan {:fore "01;36" :back "1;46"})
-(def white {:fore "01;37" :back "1;47"})
+(def black {:fore "02;30" :back "02;40"})
+(def red {:fore "02;31" :back "02;41"})
+(def green {:fore "02;32" :back "02;42"})
+(def yellow {:fore "02;33" :back "02;43"})
+(def blue {:fore "02;34" :back "02;44"})
+(def purple {:fore "02;35" :back "02;45"})
+(def cyan {:fore "02;36" :back "02;46"})
+(def lightgrey {:fore "02;37" :back "02;47"})
+(def darkgrey {:fore "01;30" :back "01;40"})
+(def boldred {:fore "01;31" :back "01;41"})
+(def boldgreen {:fore "01;32" :back "01;42"})
+(def boldyellow {:fore "01;33" :back "01;43"})
+(def boldblue {:fore "01;34" :back "01;44"})
+(def boldpurple {:fore "01;35" :back "01;45"})
+(def boldcyan {:fore "01;36" :back "01;46"})
+(def white {:fore "01;37" :back "01;47"})
 
 (defn create-new-colour
 	"Create a new colour from existing colours"
@@ -37,7 +37,7 @@
 (defn add-colour
 	"Add foreground and background colours to a string"
 	[{foreground :fore} {background :back} text]
-	(str "\u001b[" background "m\u001b[" foreground "m" text "\u001b[m\u001b[m"))
+	(str "\u001b[" background ";" foreground "m" text "\u001b[m"))
 
 (defn create-painter
 	"Partially apply colours to the add-colour function"
